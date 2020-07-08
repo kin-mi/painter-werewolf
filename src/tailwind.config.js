6c2de0e14b30956cd121bf839e7bb39be0a0c5f3
Kin mi <backtothenearfuture@gmail.com>
2020-07-08 14:21:33 +0900

    purge css お試し

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -42,15 +42,12 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.ts',
-      'nuxt.config.ts',
-    ],
-  },
+  // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+  purge: [
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'plugins/**/*.ts',
+    'nuxt.config.ts',
+  ],
 }
