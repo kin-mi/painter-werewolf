0a46f3d322ae07ad3d0d037ba6a06e76387e1f4a
Kin mi <backtothenearfuture@gmail.com>
2020-07-08 13:47:37 +0900

    お試し purge enable false

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -42,15 +42,18 @@ module.exports = {
   },
   variants: {},
   plugins: [],
+  // purge: {
+  //   // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+  //   enabled: process.env.NODE_ENV === 'production',
+  //   content: [
+  //     'components/**/*.vue',
+  //     'layouts/**/*.vue',
+  //     'pages/**/*.vue',
+  //     'plugins/**/*.js',
+  //     'nuxt.config.js',
+  //   ],
+  // },
   purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
-    ],
+    enabled: false,
   },
 }
