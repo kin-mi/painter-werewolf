09a93bf94e4194482e1438e51d970dc8c4df0eaf
Kin mi <backtothenearfuture@gmail.com>
2020-07-08 13:58:35 +0900

    fixed purge option

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -42,18 +42,15 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-  // purge: {
-  //   // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-  //   enabled: process.env.NODE_ENV === 'production',
-  //   content: [
-  //     'components/**/*.vue',
-  //     'layouts/**/*.vue',
-  //     'pages/**/*.vue',
-  //     'plugins/**/*.js',
-  //     'nuxt.config.js',
-  //   ],
-  // },
   purge: {
-    enabled: false,
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.ts',
+      'nuxt.config.ts',
+    ],
   },
 }
